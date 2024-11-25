4fa969f323759c7fdcbfa3abbb648ef7db10bb08
froji <dfroji@proton.me>
2024-11-25 22:28:39 +0200

    adjusted the names of the test cases for chunk.js

--- tests/chunk.test.js
+++ tests/chunk.test.js
@@ -3,15 +3,15 @@ import { describe, test, expect } from "vitest";
 import chunk from "src/chunk.js";
 
 describe("chunk.js", () => {
-  test("Array of length 4 split into chunks of length 2", async () => {
+  test("Array of length 4 split into chunks of length 2, expecting two arrays of length 2", async () => {
     expect(chunk(['a', 'b', 'c', 'd'], 2)).toEqual([['a', 'b'], ['c', 'd']]);
   });
 
-  test("Array of length 4 split into chunks of length 3", async () => {
+  test("Array of length 4 split into chunks of length 3, expecting an array of length 3 and an array of length 1", async () => {
     expect(chunk(['a', 'b', 'c', 'd'], 3)).toEqual([['a', 'b', 'c'], ['d']]);
   });
 
-  test("Array of length 0 split into chunks of length 2", async () => {
+  test("Array of length 0 split into chunks of length 2, expecting an array of length 0", async () => {
     expect(chunk([], 2)).toEqual([]);
   });
 

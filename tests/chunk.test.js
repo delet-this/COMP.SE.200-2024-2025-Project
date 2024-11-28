d00ab2ba9cda5b6942253f6e8ca94623f7504e84
froji <dfroji@proton.me>
2024-11-28 15:22:42 +0200

    added test cases and adjusted test names for chunk()

--- tests/chunk.test.js
+++ tests/chunk.test.js
@@ -3,19 +3,31 @@ import { describe, test, expect } from "vitest";
 import chunk from "src/chunk.js";
 
 describe("chunk.js", () => {
-  test("Array of length 4 split into chunks of length 2, expecting two arrays of length 2", async () => {
+  test("Even chunks", async () => {
     expect(chunk(['a', 'b', 'c', 'd'], 2)).toEqual([['a', 'b'], ['c', 'd']]);
+    expect(chunk(['a', 'b', 'c', 'd', 'e', 'f'], 2)).toEqual([['a', 'b'], ['c', 'd'], ['e', 'f']]);
   });
 
-  test("Array of length 4 split into chunks of length 3, expecting an array of length 3 and an array of length 1", async () => {
+  test("Uneven chunks", async () => {
     expect(chunk(['a', 'b', 'c', 'd'], 3)).toEqual([['a', 'b', 'c'], ['d']]);
+    expect(chunk(['a', 'b', 'c', 'd', 'e'], 2)).toEqual([['a', 'b'], ['c', 'd'], ['e']]);
   });
 
-  test("Array of length 0 split into chunks of length 2, expecting an array of length 0", async () => {
+  test("Chunk longer than given array", async () => {
+    expect(chunk(['a', 'b'], 3)).toEqual(['a', 'b']);
+    expect(chunk(['a', 'b', 'c'], 6)).toEqual(['a', 'b', 'c']);
+  });
+
+  test("Null array", async () => {
     expect(chunk([], 2)).toEqual([]);
   });
 
-  test("Array of length 2 split into chunks of 3, expecting array of length 2", async () => {
-    expect(chunk(['a', 'b'], 3)).toEqual(['a', 'b']);
+  test("Negative chunk size", async () => {
+    expect(chunk(['a', 'b'], -1)).toEqual([]);
+  });
+
+  test("Default chunk size", async () => {
+    expect(chunk(['a', 'b'])).toEqual([['a'], ['b']]);
+    expect(chunk(['a', 'b', 'c'])).toEqual([['a'], ['b'], ['c']]);
   });
 });

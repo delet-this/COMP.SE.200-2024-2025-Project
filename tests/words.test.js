9af3e8b0e54d51e78e11f26764c512eaab0466a5
froji <dfroji@proton.me>
2024-11-28 15:29:50 +0200

    adjusted test names for words()

--- tests/words.test.js
+++ tests/words.test.js
@@ -3,23 +3,25 @@ import { describe, test, expect } from "vitest";
 import words from "src/words.js";
 
 describe("words.js", () => {
-  test("Input with only alphanumerics", async () => {
+  test("Alphanumerics", async () => {
     expect(words("The 10 quick brown foxes")).toEqual(["The", "10", "quick", "brown", "foxes"]);
   });
 
-  test("Input with words using non-standard latin alphabets", async () => {
+  test("Non-standard latin alphabets", async () => {
     expect(words("fåå bär føøbãr")).toEqual(["fåå", "bär", "føøbãr"]);
   });
 
-  test("Input with punctuation", async () => {
+  test("Punctuation", async () => {
     expect(words("foo, bar, foobar.")).toEqual(["foo", "bar", "foobar"]);
+    expect(words("foo,bar,foobar.")).toEqual(["foo", "bar", "foobar"]);
   });
 
-  test("Input with special characters", async () => {
+  test("Special characters", async () => {
     expect(words("foo & bar 10%")).toEqual(["foo", "bar", "10"]);
+    expect(words("foo&bar10%")).toEqual(["foo", "bar", "10"]);
   });
 
-  test("Input with special characters, using a regex", async () => {
+  test("Using regex", async () => {
     expect(words("foo & bar 10%", /[a-z]+|\d+%*/g)).toEqual(["foo", "bar", "10%"]);
   });
 });

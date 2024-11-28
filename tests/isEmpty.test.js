87e1a8abfa7fa30b6cf754dcbd8a5799d25c4695
froji <dfroji@proton.me>
2024-11-28 15:26:14 +0200

    added and categorized test cases for isEmpty()

--- tests/isEmpty.test.js
+++ tests/isEmpty.test.js
@@ -3,15 +3,32 @@ import { describe, test, expect } from "vitest";
 import isEmpty from "src/isEmpty.js";
 
 describe("isEmpty.js", () => {
-  test("Null input, expecting true", async () => {
+  test("Values with no enumerable properties", async () => {
     expect(isEmpty(null)).toBe(true);
+    expect(isEmpty(true)).toBe(true);
+    expect(isEmpty(false)).toBe(true);
+    expect(isEmpty(1)).toBe(true);
   });
 
-  test("String input, expecting false", async () => {
-    expect(isEmpty("abc")).toBe(false);
-  });
+  describe("Enumerable objects", () => {
+    test("String", async () => {
+      expect(isEmpty("abc")).toBe(false);
+      expect(isEmpty("")).toBe(true);
+    });
 
-  test("Zero-length string input, expecting true", async () => {
-    expect(isEmpty("")).toBe(true);
+    test("Array", async () => {
+      expect(isEmpty([1, 2, 3])).toBe(false);
+      expect(isEmpty([])).toBe(true);
+    });
+
+    test("Map", async () => {
+      expect(isEmpty({"foo": "bar"})).toBe(false);
+      expect(isEmpty(new Map([]))).toBe(true);
+    });
+
+    test("Set", async () => {
+      expect(isEmpty(new Set([1, 2, 3]))).toBe(false);
+      expect(isEmpty(new Set([]))).toBe(true);
+    });
   });
 });
